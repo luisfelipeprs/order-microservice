cae26ab7cf35287e5ee11883a18cb3ffe8cc337a
luisfelipeprs <felipepereiracript@gmail.com>
2025-05-11 21:18:32 -0300

    chore: update package.json to include ts-node-dev and configure dotenv for database connection
    - Added ts-node-dev as a dependency for improved development experience.
    - Updated database configuration to use environment variables for DB_USER and DB_PASSWORD.
    - Added dotenv configuration to load environment variables in database and index files.

--- payment-service/src/config/database.ts
+++ payment-service/src/config/database.ts
@@ -1,12 +1,20 @@
+import 'dotenv/config'
+
 import { DataSource } from 'typeorm'
 import { Payment } from '../database/payment.entity'
+
+console.log({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
+});
 
 export const AppDataSource = new DataSource({
   type: "mysql",
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.PASSWORD,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   synchronize: true,
   entities: [Payment],
--- payment-service/src/index.ts
+++ payment-service/src/index.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config'
 import express from 'express';
 import { AppDataSource } from './config/database';
 import { consumerOrderCreated } from './kafka/consumer';
